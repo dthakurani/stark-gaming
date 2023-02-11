3d20b031fa82331a831d9427e891fd7ca2a6816d
divyanshigkmit <divyanshi@gkmit.co>
2023-02-11 20:59:50 +0530

    Feat: Create database connection

--- index.js
+++ index.js
@@ -2,9 +2,12 @@
 require('dotenv').config();
 
 const app = require('./app');
+const { sequelize } = require('./models');
 
 const startServer = async () => {
   try {
+    sequelize.authenticate();
+    console.log('... Service db ✔');
     app.listen(process.env.SERVER_PORT);
     console.log(`--- Server started on ${process.env.SERVER_PORT} ---\n\n`);
   } catch (err) {
